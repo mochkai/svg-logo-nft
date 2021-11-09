5f6a4872a86e8372afd88d2edf5775500c5c9721
MochKai <thiago.amerssonis@gmail.com>
2021-11-09 08:50:54 +0000

    Fixed IPFS

--- scripts/gen/test.ts
+++ scripts/gen/test.ts
@@ -1,9 +1,10 @@
 import { AutoGen } from './autoGen';
 
-function main() {
+async function main() {
   let gen = new AutoGen(10);
   //gen = new AutoGen(1000);
 
+  await gen.initIPFS();
   gen.setBaseSVG('assets/baseSVG.svg');
   gen.generateMetadataAttributes();
   gen.generateMetadata();
